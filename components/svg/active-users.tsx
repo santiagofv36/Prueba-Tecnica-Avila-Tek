c9b93cdb372a08218a1eec95f7b3e5cae5d7490d
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-01-18 10:18:37 -0400

    Bug fix
    Error con la posicion de la imagen de Usuarios activos

--- components/svg/active-users.tsx
+++ components/svg/active-users.tsx
@@ -3,11 +3,10 @@ import active from "@/assets/ActiveUsers.png";
 
 export default function ActiveUsers() {
   return (
-  <Image
-    alt="Active Users"
-    src={active}
-    className="w-[150px] absolute bottom-2 right-0 sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px]"
-  />
+    <Image
+      alt="Active Users"
+      src={active}
+      className="w-[150px] absolute bottom-2 right-0 sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px] xl:right-64 2xl:right-80"
+    />
   );
 }
-
