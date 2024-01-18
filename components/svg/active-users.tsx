f1e6c65929b2235741ee90de3f1e8435d2ebba03
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-01-18 13:32:45 -0400

    Fix de Active users para pantallas 2xl

--- components/svg/active-users.tsx
+++ components/svg/active-users.tsx
@@ -6,7 +6,7 @@ export default function ActiveUsers() {
     <Image
       alt="Active Users"
       src={active}
-      className="w-[150px] absolute bottom-2 right-0 sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px] xl:right-64 2xl:right-80"
+      className="w-[150px] absolute bottom-2 right-0 sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px] xl:right-64 2xl:right-96"
     />
   );
 }
